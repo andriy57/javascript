90e13be77d91923cccf9f2d17d8cab3be509fa6e
andriy57 <kuzykandriy57@gmail.com>
2019-08-17 17:08:38 +0000

    Sample code

---
+++ js/index.js
@@ -0,0 +1,11 @@
+alert (`Hello JavaScript!`);
+
+let userName = prompt(`Enter your name`);
+let greeting = `Hello`
+alert(greeting + userName);
+
+
+// запитати вік користувача 
+// вивести вік на екран 
+// Зробити змінну, в яку записати вік +1
+// Вивести на екран вік, який буде в наступному році.
